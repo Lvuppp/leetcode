e68c951a71f788ea5eaab342ddead0f04e4531be
Lvuppp <egor.bagrovets.eb@gmail.com>
2023-07-04 18:01:34 +0300

    better solution for SearchInRotatedSortedArray

--- SearchInRotatedSortedArray/SearchInRotatedSortedArray.cpp
+++ SearchInRotatedSortedArray/SearchInRotatedSortedArray.cpp
@@ -7,43 +7,6 @@ using namespace std;
 int search(vector<int>& nums, int target) {
     int start = 0, end = nums.size() - 1, middle = (start + end) / 2;
 
-    if (nums[0] > nums[nums.size() - 1]) {
-        if (nums.size() > 2) {
-            while (end >= start) {
-                middle = (start + end) / 2;
-
-                if (middle == nums.size() - 1 || nums[middle] < nums[middle + 1] && nums[middle] < nums[middle - 1]) {
-                    if (target >= nums[0]) {
-                        start = 0;
-                        end = middle - 1;
-                    }
-                    else {
-                        start = middle;
-                        end = nums.size() - 1;
-                    }
-                    break;
-                }
-
-                if (nums[middle] < nums[0]) {
-                    end = middle - 1;
-                }
-                else {
-                    start = middle + 1;
-                }
-            }
-        }
-        else {
-            for (size_t i = 0; i < nums.size(); i++)
-            {
-                if (nums[i] == target) {
-                    return i;
-                }
-            }
-            return -1;
-        }
-        
-    }
-
     while (end >= start) {
         middle = (start + end) / 2;
 
@@ -51,11 +14,13 @@ int search(vector<int>& nums, int target) {
             return middle;
         }
 
-        if (nums[middle] > target) {
-            end = middle - 1;
+        if (nums[middle] > target && target < nums[0] && nums[middle] >= nums[0] ||
+            nums[middle] < target && target < nums[0] && nums[middle] <= nums[0] ||
+            nums[middle] < target && target > nums[0] && nums[middle] >= nums[0]) {
+            start = middle + 1;
         }
         else{
-            start = middle + 1;
+            end = middle - 1;
         }
     }
 
@@ -64,7 +29,11 @@ int search(vector<int>& nums, int target) {
 
 int main()
 {
+    vector<int> vec{1,3};
 
-    vector<int> vec{4, 5, 6, 7, 9, 10, 2};
-    cout << search(vec, 5);
+    for (size_t i = 0; i < vec.size(); i++)
+    {
+        cout << search(vec, vec[i]);
+    }
+    cout << search(vec, 3);
 }
